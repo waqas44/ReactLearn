5f27724280fee0372c042c80d4ac8de7ba5e5460
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-04-19 18:41:15 +0500

    test 3

--- src/App.js
+++ src/App.js
@@ -30,9 +30,13 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount}
-      ></ExpenseItem>
+        date={expenses[0].date}></ExpenseItem>
 
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount}
+        date={expenses[1].date}></ExpenseItem>
 
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount}
+        date={expenses[2].date}></ExpenseItem>
 
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
 
   return (
     <div className='expense-item'>
-      <div>{props.date}</div>
+      <div> {month} &nbsp; </div>
+      <div> {year} &nbsp;</div>
+      <div> {day} &nbsp;</div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
