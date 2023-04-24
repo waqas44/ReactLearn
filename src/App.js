09b5bd50f9b59d3a1493d4907d1b1dc054777420
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-04-24 20:42:53 +0500

    Working code test 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import ExpenseItem from './components/ExpenseItem';
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
 
   const expenses = [
     {
@@ -29,14 +30,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount}
-        date={expenses[0].date}></ExpenseItem>
-
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount}
-        date={expenses[1].date}></ExpenseItem>
-
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount}
-        date={expenses[2].date}></ExpenseItem>
+      <Expenses items={expenses} />
 
     </div>
   );
