d4d95ef2323a867557c46fb49af0f2a5e94afba8
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-04-25 21:05:23 +0500

    Working with status test 1

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
 
+  const [nTitle, updateTitle] = useState(props.title);
+  const clickHandle = () => {
+    // let props.title = "New Title";
+    updateTitle('updated title');
+    console.log(props.title);
+
+  };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{nTitle}</h2>
         <div className='expense-item__price'>${props.amount}</div>
+
       </div>
+      <button onClick={clickHandle}>Update Title Btn</button>
     </Card>
   );
 }
