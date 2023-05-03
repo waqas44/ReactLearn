afedc12b20a983181f0b3da7740095eddf831a35
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-05-03 16:45:38 +0500

    child to parent  component communication test 1

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
@@ -44,7 +44,8 @@ const ExpenseForm = () => {
             date: enteredDate
 
         };
-        console.log(NewExpense);
+        // console.log(NewExpense);
+        props.onSaveExpenseData();
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,9 +4,22 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = () => {
+
+    function saveExpenseDataHandler(enteredExpenseData) {
+
+        const expenseData = {
+
+            ...enteredExpenseData,
+            id: Math.random().toString()
+
+        };
+        console.log(expenseData);
+    };
+
+
     return (
         <div className='new-expense'>
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
         </div>
     );
 };
