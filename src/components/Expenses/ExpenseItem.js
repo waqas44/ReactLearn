cdfa2c00ad0bc55885af45116cf829a2d2bc3de8
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-05-08 20:59:02 +0500

    Using Stateful Lists Test 3

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -23,7 +23,7 @@ const ExpenseItem = (props) => {
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{nTitle}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
 
       </div>
