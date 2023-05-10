cd5623a92265570f8e0534e8b1bbaca9d388a27e
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-05-10 21:13:51 +0500

    Demo App: Adding a Chart not working test 1

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import './Chart.css';
+import ChartBar from "./ChartBar";
+
+import './ChartBar.css';
+const Chart = (props) => {
+    const
+
+    return (
+        <div className="chart">
+
+            {props.dataPoints.map((dataPoints) => (
+                <ChartBar
+                    key={dataPoints.label}
+                    value={dataPoints.value}
+                    maxValue={null}
+                    label={dataPoints.label}
+                />
+
+            )
+            )}
+
+        </div>
+    );
+
+};
+export default Chart;
