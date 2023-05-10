fbe5608ac040a48d66b6a077e7ca17344becd2e8
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-05-10 15:48:15 +0500

    Adding conditional return statment test 2 working

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,17 +1,17 @@
 import React from "react";
-
-import Expenses from "./Expenses";
 import ExpenseItem from "./ExpenseItem";
+
+import './ExpensesList.css';
 const ExpensesList = (props) => {
 
 
-    if (props.items.length == 0) {
-        return <h2 className="abc" > Nothing found </h2>
+    if (props.items.length === 0) {
+        return <h2 className='expenses-list__fallback'> Nothing found </h2>
 
     }
 
     return (
-        <ul className="ac">
+        <ul className='expenses-list'>
             {props.items.map((expense) =>
             (<ExpenseItem
                 key={expense.id}
